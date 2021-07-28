f82404df29a542d5aabf7e8d64926f16b5cc6cb1
Siah <i@siah.sg>
2021-07-28 16:14:51 +0800

    livekit initial commit

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
+const livekitApi = require("livekit-server-api");
+require("dotenv").config();
+const AccessToken = livekitApi.AccessToken;
+const RoomServiceClient = livekitApi.RoomServiceClient;
 const express = require("express");
-const { ExpressPeerServer } = require("peer");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-const server = app.listen(9000);
-const peerServer = ExpressPeerServer(server, { path: "/myapp", debug: true });
+const livekitHost = "http://localhost:7880";
+const svc = new RoomServiceClient(
+  livekitHost,
+  process.env.LIVEKIT_API_KEY,
+  process.env.LIVEKIT_API_SECRET
+);
 
-app.use("/peerjs", peerServer);
-
-peerServer.on("connection", (client) => {
-  let { id, token } = client;
-  console.log({ id, token });
+app.get("/", (req, res) => {
+  const roomName = "name-of-room";
+  const participantName = "user-name";
+  const at = new AccessToken(
+    process.env.LIVEKIT_API_KEY,
+    process.env.LIVEKIT_API_SECRET,
+    {
+      identity: participantName,
+    }
+  );
+  at.addGrant({
+    roomJoin: true,
+    room: roomName,
+    canPublish: false,
+    canSubscribe: true,
+  });
+  const token = at.toJwt();
+  res.send({ token });
 });
 
-peerServer.on("disconnect", (client) => {
-  console.log(client.id, client.token, "disconnected");
+app.get("/rooms", (req, res) => {
+  svc.listRooms().then((result) => res.send(result));
 });
+
+app.get("/participants", (req, res) => {
+  svc
+    .listParticipants("name-of-room")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => res.send({ err }));
+});
+
+app.listen(3001);
